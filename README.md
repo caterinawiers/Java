cf1125b1967a5f294aad583c7d1262ce54128333
Caterina Wiers <caterinawps@gmail.com>
2023-10-09 11:07:46 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java
+ CUrso Rocketseat Java
---
+++ todolist/src/main/java/br/com/danieleleao/todolist/controller/MinhaPrimeiraController.java
@@ -0,0 +1,19 @@
+package br.com.danieleleao.todolist.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MinhaPrimeiraController {
+
+/**
+ * GET - 
+ * POST - 
+ * PUT - 
+ * DELETE - 
+ * PATCH - 
+ *  */
+  
+  public String primeiraMensagem() {
+    return "Funcionou";
+  }
+}
